7c0ff60783eb99d594d4e32749def4fb4af85efe
mmacedoaraujo <marcomacedoaraujo@gmail.com>
2023-01-31 09:55:48 -0300

    tested: repository with manually instantiated data

--- src/main/java/com/mmacedo/crudspring/CrudSpringApplication.java
+++ src/main/java/com/mmacedo/crudspring/CrudSpringApplication.java
@@ -1,13 +1,29 @@
 package com.mmacedo.crudspring;
 
+import com.mmacedo.crudspring.model.Course;
+import com.mmacedo.crudspring.repository.CourseRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class CrudSpringApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CrudSpringApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(CrudSpringApplication.class, args);
+    }
 
+    @Bean
+    CommandLineRunner initDatabase(CourseRepository courseRepository) {
+        return args -> {
+            courseRepository.deleteAll();
+
+            Course course = new Course();
+            course.setName("Angular com Spring");
+            course.setCategory("Full Stack");
+
+            courseRepository.save(course);
+        };
+    }
 }
--- src/main/java/com/mmacedo/crudspring/controller/CourseController.java
+++ src/main/java/com/mmacedo/crudspring/controller/CourseController.java
@@ -1,6 +1,10 @@
 package com.mmacedo.crudspring.controller;
 
 import com.mmacedo.crudspring.model.Course;
+import com.mmacedo.crudspring.repository.CourseRepository;
+import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,10 +14,15 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/courses")
-public class HelloController {
+@AllArgsConstructor
+public class CourseController {
+
+    private final CourseRepository courseRepository;
 
     @GetMapping
     public ResponseEntity<List<Course>> listAll() {
-        return null;
+        List<Course> allCourses = courseRepository.findAll();
+
+        return new ResponseEntity<>(allCourses, HttpStatus.OK);
     }
 }
