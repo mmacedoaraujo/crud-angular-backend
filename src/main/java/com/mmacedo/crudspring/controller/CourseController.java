54955d037386c8734f11e4855176f51dd1e48d89
mmacedoaraujo <marcomacedoaraujo@gmail.com>
2023-02-06 19:51:06 -0300

    testing: endpoint backend connection with frontend

--- src/main/java/com/mmacedo/crudspring/controller/CourseController.java
+++ src/main/java/com/mmacedo/crudspring/controller/CourseController.java
@@ -6,9 +6,7 @@ import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -24,5 +22,9 @@ public class CourseController {
         List<Course> allCourses = courseRepository.findAll();
 
         return new ResponseEntity<>(allCourses, HttpStatus.OK);
+    }
+    @PostMapping
+    public void create(@RequestBody Course course) {
+        System.out.println(course.getName());
     }
 }
